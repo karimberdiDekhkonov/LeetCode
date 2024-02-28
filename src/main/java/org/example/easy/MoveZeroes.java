083cd04acc9075c2d49270302d10a2f99b4a41bb
karimberdiDekhkonov <karimberdidekhkonov0921@gmail.com>
2024-02-28 20:22:06 +0100

    MOVE ZEROES

---
+++ src/main/java/org/example/easy/MoveZeroes.java
@@ -0,0 +1,17 @@
+package org.example.easy;
+
+
+//28.02.2024
+//BY USING TWO POINTERS WE SOLVE THE PROBLEM WITH 100% EFFICIENTLY ON LEETCODE PLATFORM.
+//TIME COMPLEXITY: O(n).
+//SPACE COMPLEXITY: O(1).
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int nonZeroIndex = 0;
+
+        for(int num : nums) if(num!=0) nums[nonZeroIndex++] = num;
+
+        while(nonZeroIndex < nums.length) nums[nonZeroIndex++]=0;
+    }
+}
